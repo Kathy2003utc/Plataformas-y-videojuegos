1f457542f67d4f30b3ed0bab06e130f10687ef95
Katherin <katherin.pico2124@utc.edu.ec>
2025-06-09 21:52:39 -0500

    creacion del models Plataforma

--- Aplicaciones/Plataforma/models.py
+++ Aplicaciones/Plataforma/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Plataforma(models.Model):
+    nombre = models.CharField(max_length=50)
+    fabricante = models.CharField(max_length=50)
+    lanzamiento = models.DateField()
+    version_sistema = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
