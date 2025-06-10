e87303c38d5181e96716dd939501152de484fed3
Katherin <katherin.pico2124@utc.edu.ec>
2025-06-09 22:10:31 -0500

    views de videojuegos

--- Aplicaciones/Videojuego/models.py
+++ Aplicaciones/Videojuego/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from Aplicaciones.Plataforma.models import Plataforma
 
-# Create your models here.
+class Videojuego(models.Model):
+    titulo = models.CharField(max_length=100)
+    genero = models.CharField(max_length=50)
+    fecha_lanzamiento = models.DateField()
+    plataforma_principal = models.ForeignKey(Plataforma, on_delete=models.CASCADE, related_name='Plataforma')
+    def __str__(self):
+        return self.titulo
